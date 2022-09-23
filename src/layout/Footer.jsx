19f587031a800f6c8fb7845bf322630af5150a67
ara fa adri <arafa.sco@gmail.com>
2022-09-23 10:50:41 +0700

    footer menu utama

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -6,10 +6,50 @@ const Footer = () => {
     return (
         <div className="relative w-full min-h-20 ">
             <div className="flex justify-center py-0 px-0">
-                <div className='h-96 w-full bg-teal-600'>
-                    <div className='text-center'>
-                        <p className='text-3xl my-10 text-white'>KOPKARLA</p>
-                        <hr />
+                <div className='h-96 w-full flex '>
+                    {/* <div className='w-1/5 h-full bg-teal-800 px-10 pt-11'>
+                        <Logo className="max-h-20 bg-white mb-4 " />
+                        <p className='text-white mb-4'>HEAD OFFICE</p>
+                        <p className='text-white text-sm mb-4 leading-8'>
+                        Gedung KOPKARLA 1
+                        Jl. Lebak Bulus Raya No.7
+                        Kel. Lebak Bulus Kec. Cilandak
+                        Jakarta Selatan, DKI Jakarta 12440
+                        </p>
+                    </div> */}
+                    {/* <div className='w-1/5 h-full bg-teal-700 px-10 pt-[3rem]'>
+                        <p className='text-white text-xl leading-10 mb-5'>
+                            Contact Us :
+                        </p>
+                    <p className='text-white text-sm leading-10'>
+                        Telp.  021 7590 9424<br/>
+                        <hr/>
+                        Fax.  021 2920 3466<br/>
+                        <hr/>
+                        info  @kopkarla.co.id
+                        <hr/>
+                        </p>
+                    </div> */}
+                    <div className='w-full h-full bg-teal-600 px-10 pt-[3rem] '>
+                        <p className='lg:mx-3 text-xl text-white'>MENU UTAMA <hr /></p>
+                        <ul className='flex'>
+                            {Menus.map((menu, index)=>(
+                                <li key={index} className='lg:mx-9 text-xs text-white my-4'>
+                                    {menu.name} <hr />
+                                    <ul>
+                                    {menu.subs ? 
+                                    menu.subs.map((sub, index)=>(
+                                        
+                                            <li className='my-3'>{sub.name} <hr /></li>
+    
+                                    ))
+                                    :
+                                    ''
+                                }
+                                    </ul>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
                 </div>
                 {/* <div className='w-1/3 px-8 py-8'>
@@ -23,7 +63,7 @@ const Footer = () => {
                 ))}
                 </ul> */}
             </div>
-            <div className='text-white text-center bg-white bg-teal-600 py-10'>
+            <div className='text-sm text-white text-center bg-gradient-to-t from-teal-800 to-teal-600 py-3'>
                 COPYRIGHT © 2019 KOPERASI KONSUMEN KARYAWAN PT APLIKANUSA LINTASARTA
             </div>
         </div>
--- src/layout/navbar/Logo.jsx
+++ src/layout/navbar/Logo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Logo = (props) => {
   const {children, className} = props;
   return (
-    <div className="cursor-pointer items-center flex">
+    <div className="cursor-pointer items-center">
         <img className={className} src={require('../../assets/images/logo.png')} />
     </div>
   )
